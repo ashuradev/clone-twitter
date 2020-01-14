06cbad67337eae0237b2c2638c42d969e3da76ae
Ashura <the.lucas2016@gmail.com>
2020-01-13 23:43:16 -0200

    !feat: Replace RegisterController with UserController::store

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
-    Route::post('register', 'RegisterController@store');
     Route::post('login', 'LoginController@login');
     Route::post('logout', 'LoginController@logout');
     Route::post('refresh', 'LoginController@refresh');
 });
 
+Route::post('/users', 'UserController@store');
+
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('posts', 'PostController');
-    Route::get('feed', 'FeedController@index');
 });
