ab12f48fc68ddd546f050fc0e07900c71d82dd2d
Ashura <the.lucas2016@gmail.com>
2020-01-18 21:02:21 -0200

    Some changes on auth

--- web/src/components/Form/Button.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Button = styled.button`
-  background: rgb(29, 161, 242);
-  color: #fff;
-  font-size: 15px;
-`;
-
-export default Button;
--- web/src/pages/Auth/Login.js
+++ web/src/pages/Auth/Login.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import Logo from '../../components/Logo';
-import Button from '../../components/Form/Button';
-
 const Login = () => {
   const onSubmit = e => {
     e.preventDefault();
@@ -10,12 +7,8 @@ const Login = () => {
 
   return (
     <>
-      <Logo />
-
       <form onSubmit>
         <h1>Entrar</h1>
-
-        <Button type="submit">Entrar</Button>
       </form>
     </>
   );
