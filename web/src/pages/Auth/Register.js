5ff94c5facd5f413a3b5282ea84ebb8a5c65e2db
Ashura <the.lucas2016@gmail.com>
2020-01-19 20:33:11 -0200

    :sparkles: feat(auth): Register logic finished

--- web/src/pages/Auth/Register.js
+++ web/src/pages/Auth/Register.js
@@ -1,55 +1,85 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Link from '../../components/Link';
+import InputField from './components/InputField';
 
-import { Wrapper, Logo, Title, Button, Input } from './styles';
+import { Wrapper, Form, Logo, Title, Button } from './styles';
 
 import logo from '../../assets/images/logo.png';
 
+import api from '../../services/api';
+
 const Register = () => {
+  const history = useHistory();
+
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
+
+    try {
+      await api.post('/users', {
+        name,
+        username,
+        email,
+        password
+      });
+    } catch ({ response }) {
+      response.status === 422 && setErrors(response.data.errors);
+    }
+
+    const {
+      data: { token }
+    } = await api.post('/auth/login', { email, password });
+
+    localStorage.setItem('token', token);
+
+    history.push('/');
   };
 
   return (
     <Wrapper>
       <Logo src={logo} />
 
-      <form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit}>
         <Title>Criar uma conta</Title>
 
-        <Input
+        <InputField
           type="text"
           value={name}
           placeholder="Seu nome"
           onChange={e => setName(e.target.value)}
+          error={errors.name}
         />
-        <Input
+        <InputField
           type="text"
           value={username}
           placeholder="Seu nome de usuário"
           onChange={e => setUsername(e.target.value)}
+          error={errors.username}
         />
-        <Input
+        <InputField
           type="email"
           value={email}
           onChange={e => setEmail(e.target.value)}
           placeholder="Seu e-mail"
+          error={errors.email}
         />
-        <Input
+        <InputField
           type="password"
           value={password}
           onChange={e => setPassword(e.target.value)}
           placeholder="Sua senha"
+          error={errors.password}
         />
 
         <Button type="submit">Criar uma conta</Button>
-      </form>
+      </Form>
 
       <Link to="/login">Você já possui uma conta?</Link>
     </Wrapper>
